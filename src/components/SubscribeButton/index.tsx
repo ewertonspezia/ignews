0ab5adba2c59355b2a2e9e00c8fa1977d195958a
ewertonspezia <ewertonspezia@outlook.com>
2021-10-25 11:13:53 -0300

    Configuracoes dos webhooks da aplicacao pelo stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -19,7 +19,7 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
         try{
             const response = await api.post('/subscribe')
 
-            const sessionId  = response.data;
+            const { sessionId }  = response.data;
 
             const stripe = await getStripeJs();
 
